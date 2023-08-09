d790a3d049689d131dc20b64d9e0a1825af6eb96
ssoonan <ssoonan0770@gmail.com>
2023-08-09 23:26:55 +0900

    extract word_counter function

--- main.c
+++ main.c
@@ -2,16 +2,28 @@
 #include <string.h>
 
 
+int word_counter(const char* words) {
+  char copied[strlen(words)];
+  strcpy(copied, words);
+  int counter = 0;
+
+  const char delimiters[] = " ";
+  char *token;
+
+  token = strtok(copied, delimiters);
+
+  while (token != NULL) {
+    token = strtok(NULL, delimiters);
+    counter++;
+  }
+  return counter;
+}
 
 int main(int argc, char const *argv[])
 {
-  char * words = "there are many words in this string";
-  int length = strlen(words);
-  for (size_t i = 0; i < length; i++)
-  { 
-    printf(words[i]);
-  }
-  
+  const char* words = "there are many words in this string";
+  int counter = word_counter(words);
+  printf("words: %d", counter);
 
   return 0;
 }
